31ac5a7707dab1a53a5d732e5e72efb9d2f53592
aweiteka <aweiteka@redhat.com>
2014-09-18 12:03:48 -0400

    return proof of concept data structure from some real data

--- check_dockerfile.py
+++ check_dockerfile.py
@@ -2,31 +2,48 @@
 
 import argparse
 import re
-#from json import JSONEncoder
 import json
+
+DOCS_URL = "https://docs.docker.com/reference/builder/"
 
 class Dockerfile(object):
 
     def __init__(self, dockerfile):
         self.dockerfile = dockerfile
-        self.f = open(self.dockerfile, 'r')
+        self._raw_file = []
         self._layer_count = 0
-        self._from_val = ""
+        self._from_val = None
+        self._maintainer = None
         self.process_dockerfile()
 
     def process_dockerfile(self):
-        for line in self.f:
-            p = re.compile(r'FROM (.+$)')
-            from_line = p.match(line)
-            self.incr_layer_count()
-            if from_line:
-                self.set_from_val(from_line.group(1))
+        with open(self.dockerfile, 'r') as f:
+            for line in f:
+                self.set_raw_file(line.strip())
+                self.parse_dockerfile(line)
+
+    def parse_dockerfile(self, line):
+        p = re.compile(r'(\w+)\s(.+$)')
+        m = p.match(line)
+        if m:
+            self.incr_layer_count(line)
+            if "FROM" in m.group(1):
+                self.set_from(m.group(2))
+            if "MAINTAINER" in m.group(1):
+                self.set_maintainer(m.group(2))
+
+    @property
+    def raw_file(self):
+        return self._raw_file
+
+    def set_raw_file(self, f):
+        self._raw_file.append(f)
 
     @property
     def from_val(self):
         return self._from_val
 
-    def set_from_val(self, val):
+    def set_from(self, val):
         self._from_val = val
 
     @property
@@ -44,37 +61,100 @@ class Dockerfile(object):
             return False
 
     @property
+    def maintainer(self):
+        return self._maintainer
+
+    def set_maintainer(self, maintainer=None):
+        self._maintainer = maintainer
+
+    @property
+    def has_maintainer(self):
+        if self.maintainer:
+            return True
+        else:
+            return False
+
+    @property
     def layer_count(self):
         return self._layer_count
 
-    def incr_layer_count(self):
-        self._layer_count = self._layer_count + 1
+    def incr_layer_count(self, line):
+        if "#" not in line:
+            self._layer_count = self._layer_count + 1
 
-    def print_summary(self):
-        print self.from_val
-        print "latest? %s" % self.is_latest
-        print "tag? %s" % self.has_tag
-        print self.layer_count
+    @property
+    def summary(self):
+        return {"raw": self.raw_file,
+                "FROM": self.from_val,
+                "layer_count": self.layer_count
+                }
+
+    @property
+    def info(self):
+        info = dict()
+        info.update({"Maintainer": self.maintainer})
+        if self.is_latest:
+            info.update({"from_latest": {
+                                "line": 1,
+                                "message": "base image uses 'latest' tag",
+                                "description": "using the 'latest' tag may cause unpredictable builds. It is recommended that a specific tag is used in the FROM line.",
+                                "reference_url": DOCS_URL + "#from"}
+                        })
+        return info
+
+    @property
+    def warnings(self):
+        warning = dict()
+        if not self.has_tag:
+            warning.update({"tag": {
+                                "line": 1,
+                                "message": "No tag is used",
+                                "description": "lorem ipsum tar",
+                                "reference_url": DOCS_URL + "#from"}
+                            })
+        if not self.has_tag:
+            warning.update({"maintainer": {
+                                "line": None,
+                                "message": "Maintainer is not defined",
+                                "description": "The MAINTAINER line is useful for identifying the author in the form of MAINTAINER Joe Smith <joe.smith@example.com>",
+                                "reference_url": DOCS_URL + "#maintainer"}
+                            })
+        return warning
+
+    @property
+    def errors(self):
+        return {"Foo": "bar", "Bee": "buzz"}
 
     def to_json(self):
         print json.dumps({
-                "FROM": self.from_val,
-                "latest": self.is_latest,
-                "has_tag": self.has_tag,
-                "layer_count": self.layer_count
+                "summary": self.summary,
+                "info": {
+                    "count": len(self.info),
+                    "data": self.info
+                    },
+                "warnings": {
+                    "count": len(self.warnings),
+                    "data": self.warnings
+                    },
+                "errors": {
+                    "count": len(self.errors),
+                    "data": self.errors
+                    }
                 },
                 sort_keys=True, indent=4)
 
 def main():
     """Entrypoint for script"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("dockerfile", help="Dockerfile to validate")
+    parser.add_argument("dockerfile",
+                        metavar="path/to/Dockerfile",
+                        help="Dockerfile to analyze")
     args = parser.parse_args()
 
     d = Dockerfile(args.dockerfile)
-    #d.print_summary()
     d.to_json()
 
 if __name__ == '__main__':
     main()
+
 
